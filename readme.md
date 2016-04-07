e2e136b7d884f034380087333a6b2787fb64579a
toboid <toboid@users.noreply.github.com>
2016-04-07 18:15:40 +0100

    Initial commit

--- readme.md
+++ readme.md
@@ -1,1 +1,119 @@
 # object-stream
+
+**A simplified API for working with node object streams**
+
+This module is inspired by Rod Vagg's [through2](https://github.com/rvagg/through2) module but by focusing purely on object streams is able to provide a more concise API, including support for returning a value or promise from a transform function.
+
+At it's simplest a transform stream can be created from function that returns the desired value to be queued or a promise for that value:
+
+``` javascript
+getKeyStream()
+    .pipe(objectStream(key => getObject(key)))
+    .pipe(someOtherStream)
+```
+
+callback shorthand style is also supported:
+``` javascript
+getKeyStream()
+    .pipe(objectStream((key, callback) => {
+        const obj = getObject(key);
+        callback(null, obj);
+    }))
+    .pipe(someOtherStream)
+```
+
+as is traditional style using `this`:
+``` javascript
+getKeyStream()
+    .pipe(objectStream(function (key, callback) {
+        const obj = getObject(key);
+        this.push(obj);
+        callback();
+    }))
+    .pipe(someOtherStream)
+```
+
+## API
+`objectStream([options], [transform], [flush])`
+### options
+Options to be passed to the `stream.Transform` constructor, see [here](https://nodejs.org/api/stream.html#stream_new_stream_transform_options) for available options.
+``` javascript
+getKeyStream()
+    .pipe(objectStream({highWaterMark: 6}, key => getObject(key)))
+    .pipe(someOtherStream)
+```
+
+### transform
+The transform function will be invoked with arguments `object` and `callback`.
+`object` is the callback object on the stream, `callback` indicates the end of the transform function and signal any errors.
+
+``` javascript
+getKeyStream()
+    .pipe(objectStream(function (key, callback) {
+        const obj = getObject(key);
+        this.push(obj);
+        callback();
+    }))
+    .pipe(someOtherStream)
+```
+
+`callback` can also be called with an object as a shorthand for queuing a single object on the stream:
+``` javascript
+getKeyStream()
+    .pipe(objectStream((key, callback) => {
+        const obj = getObject(key);
+        callback(null, obj);
+    }))
+    .pipe(someOtherStream)
+```
+
+Instead of using `callback` a value may be returned from the transform function, if the return value is a promise then this will be resolved and the result queued.
+``` javascript
+getKeyStream()
+    .pipe(objectStream(key => getObject(key)))
+    .pipe(someOtherStream)
+```
+
+If a transform function is not supplied it will default to a pass-through stream.
+
+### flush
+The flush function will be invoked with a single argument `done` which can be used to indicate the end of the flush function and signal any errors.
+
+``` javascript
+getKeyStream()
+    .pipe(objectStream(function (key, callback) {
+        const obj = getObject(key);
+        this.push(obj);
+        callback();
+    }, function (done) {
+        const finalObj = getFinalObj();
+        this.push(obj);
+        done();
+    }))
+    .pipe(someOtherStream)
+```
+
+Like the `callback` function, `done` supports a shorthand for queueing a single value:
+
+``` javascript
+getKeyStream()
+    .pipe(objectStream((key, callback) => {
+        const obj = getObject(key);
+        callback(null, obj);
+    }, done => {
+        const finalObj = getFinalObj();
+        done(null, obj);
+    }))
+    .pipe(someOtherStream)
+```
+
+`flush` also supports returning a value or promise, promises will be resolved and the result queued:
+``` javascript
+getKeyStream()
+    .pipe(objectStream(key => getObject(key), () => getFinalObj()))
+    .pipe(someOtherStream)
+```
+
+## License
+MIT
+
--- test/object-stream.test.js
+++ test/object-stream.test.js
@@ -1,2 +1,234 @@
 'use strict';
 
+const _ = require('lodash');
+const expect = require('chai').expect;
+const concat = require('concat-stream');
+const objectStream = require('../object-stream');
+
+describe('object-stream', function () {
+    it('uses stream options', done => {
+        const options = {highWaterMark: 5};
+        const multiplyBy5 = objectStream(options, function (multiplier, callback) {
+            expect(this._readableState.highWaterMark).to.eql(5);
+            expect(this._writableState.highWaterMark).to.eql(5);
+            callback();
+        })
+        .on('data', () => {})
+        .on('end', () => {
+            done();
+        });
+
+        multiplyBy5.write(1);
+        multiplyBy5.end();
+    });
+
+    it('works with pipe', () => {
+        const expected = [5, 10, 20];
+
+        const multiplyBy5 = objectStream((multiplier, callback) => {
+            callback(null, {value: multiplier * 5});
+        })
+        .pipe(concat({encoding: 'object'}, function (actual) {
+            expect(expected).to.eql(actual);
+            done();
+        }));
+
+        multiplyBy5.write(1);
+        multiplyBy5.write(2);
+        multiplyBy5.write(4);
+        multiplyBy5.end();
+    });
+
+    describe('transform', function () {
+        it('defaults to a pass through stream', done => {
+            const multiplyBy5 = objectStream()
+                .on('data', obj => {
+                    expect(obj).to.eql(1);
+                })
+                .on('end', () => {
+                    done();
+                })
+
+            multiplyBy5.write(1);
+            multiplyBy5.end();
+        });
+        
+        it('transforms an object stream with callback shorthand', done => {
+            const expected = [5, 10, 20];
+            const actual = [];
+
+            const multiplyBy5 = objectStream((multiplier, callback) => {
+                callback(null, {value: multiplier * 5});
+            })
+            .on('data', obj => {
+                actual.push(obj.value);
+            })
+            .on('end', () => {
+                expect(expected).to.eql(actual);
+                done();
+            })
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+
+        it('transforms object stream with callback', done => {
+            const expected = [5, 10, 20];
+            const actual = [];
+
+            const multiplyBy5 = objectStream(function(multiplier, callback) {
+                this.push({value: multiplier * 5});
+                callback();
+            })
+            .on('data', obj => {
+                actual.push(obj.value);
+            })
+            .on('end', () => {
+                expect(expected).to.eql(actual);
+                done();
+            })
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+
+        it('transforms object stream with return value shorthand', done => {
+            const expected = [5, 10, 20];
+            const actual = [];
+
+            const multiplyBy5 = objectStream(multiplier => ({value: multiplier * 5}))
+                .on('data', obj => {
+                    actual.push(obj.value);
+                })
+                .on('end', () => {
+                    expect(expected).to.eql(actual);
+                    done();
+                });
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+
+        it('transforms object stream with promise shorthand', done => {
+            const expected = [5, 10, 20];
+            const actual = [];
+
+            const multiplyBy5 = objectStream(multiplier => Promise.resolve({value: multiplier * 5}))
+                .on('data', obj => {
+                    actual.push(obj.value);
+                })
+                .on('end', () => {
+                    expect(expected).to.eql(actual);
+                    done();
+                });
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+
+        it('errors when transform function has no callback or return value', done => {
+            const multiplyBy5 = objectStream(multiplier => {})
+                .on('data', () => {
+                    done('Stream did not error.');
+                })
+                .on('error', error => {
+                    expect(error).to.eql('Transform function did not callback or return a value/promise');
+                    done();
+                });
+
+            multiplyBy5.write(1);
+            multiplyBy5.end();
+        });
+    });
+
+    describe('flush', function () {
+        it('supports flush callback shorthand', done => {
+            const expected = [5, 10, 20, 'the end'];
+            const actual = [];
+
+            const multiplyBy5 = objectStream(multiplier => multiplier * 5, callback => {
+                callback(null, 'the end');
+            })
+            .on('data', obj => {
+                actual.push(obj);
+            })
+            .on('end', () => {
+                expect(expected).to.eql(actual);
+                done();
+            })
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+
+        it('supports flush callback', done => {
+            const expected = [5, 10, 20, 'the end'];
+            const actual = [];
+
+            const multiplyBy5 = objectStream(multiplier => multiplier * 5, function (callback) {
+                this.push('the end');
+                callback();
+            })
+            .on('data', obj => {
+                actual.push(obj);
+            })
+            .on('end', () => {
+                expect(expected).to.eql(actual);
+                done();
+            })
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+
+        it('supports flush with return value shorthand', done => {
+            const expected = [5, 10, 20, 'the end'];
+            const actual = [];
+
+            const multiplyBy5 = objectStream(multiplier => multiplier * 5, () => 'the end')
+                .on('data', obj => {
+                    actual.push(obj);
+                })
+                .on('end', () => {
+                    expect(expected).to.eql(actual);
+                    done();
+                })
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+
+        it('supports flush with promise shorthand', done => {
+            const expected = [5, 10, 20, 'the end'];
+            const actual = [];
+
+            const multiplyBy5 = objectStream(multiplier => multiplier * 5, () => Promise.resolve('the end'))
+                .on('data', obj => {
+                    actual.push(obj);
+                })
+                .on('end', () => {
+                    expect(expected).to.eql(actual);
+                    done();
+                })
+
+            multiplyBy5.write(1);
+            multiplyBy5.write(2);
+            multiplyBy5.write(4);
+            multiplyBy5.end();
+        });
+    });
+});
